034b7b1bb774653c0b4ccd5d7a9bf47520b33cb8
John Doe <ayobamioyediran2018@yahoo.com>
2022-09-20 05:14:56 +0000

    vendor commit

--- lib/views/customer_home_screen.dart
+++ lib/views/customer_home_screen.dart
@@ -10,7 +10,7 @@ class CustomerHomeScreen extends StatefulWidget {
 
 class _CustomerHomeScreenState extends State<CustomerHomeScreen> {
 //Logic For The Body
-  final List<Widget> _pages = const [
+  final List<Widget> _pages = [
     HomeScreen(),
     Center(
       child: Text('Search'),
